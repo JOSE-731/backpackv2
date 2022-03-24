52e5d111e01d478f629f60eccca7bcf498046bd2
jose diaz <jose7312000@gmail.com>
2022-03-24 17:19:29 -0500

    Agregando tests al sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('contactos') }}'><i class='nav-icon la la-question'></i> Contactos</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('datos') }}'><i class='nav-icon la la-question'></i>Datos</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tests') }}'><i class='nav-icon la la-question'></i>Tests</a></li>
