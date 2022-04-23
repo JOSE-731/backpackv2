2935f78eb07cfba1eb408e0bf4dc5b09992f8da9
Jose David <jose7312000@gmail.com>
2022-04-23 16:43:43 -0500

    Limpiando sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,9 +1,9 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('contactos') }}'><i class='nav-icon la la-question'></i> Contactos</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('datos') }}'><i class='nav-icon la la-question'></i>Datos</a></li>
+<!--<li class='nav-item'><a class='nav-link' href='{{ backpack_url('datos') }}'><i class='nav-icon la la-question'></i>Datos</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tests') }}'><i class='nav-icon la la-question'></i>Tests</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tests/create') }}'><i class='nav-icon la la-question'></i>Create</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tests') }}'><i class='nav-icon la la-question'></i> Tests</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('testv2') }}'><i class='nav-icon la la-question'></i> Testv2s</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('testv2') }}'><i class='nav-icon la la-question'></i> Testv2s</a></li> -->
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('examples') }}'><i class='nav-icon la la-question'></i> Examples</a></li>
